7810b97d85f7037e4988ef2680a2e8b30247d3e5
diljithmon170 <diljithmon170@gmail.com>
2025-08-03 18:59:44 +0530

    add with marks

--- pcm_buddy/settings.py
+++ pcm_buddy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*cs4*)uql(^x=78@^s9@e3pp=n$3ibrml4rd9xida^j3+c8b3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i*cs4*)uql(^x=78@^s9@e3pp=n$3ibrml4rd9xida^j3+c8b3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
