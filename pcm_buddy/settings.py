9df1f4b0d27b6c7ac52bff2c1913f4146aae08a2
diljithmon170 <diljithmon170@gmail.com>
2025-08-05 11:40:38 +0530

    host

--- pcm_buddy/settings.py
+++ pcm_buddy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-i*cs4*)uql(^x=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://pcm-buddy.onrender.com']
 
 
 # Application definition
